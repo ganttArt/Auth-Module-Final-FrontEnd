fe8d6856d7f7c30fdd198726390955ee2f58425c
Chris Gantt <gantt.art@gmail.com>
2021-04-30 12:25:49 -1000

    Blueprint for full crud note taking

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
 'use strict';
 
 console.log('html hooked up');
+
+function signupSubmitHandler(event) {
+  event.preventDefault();
+  console.log(event.target);
+  console.log(event.target.username.value);
+  console.log(event.target.password.value);
+}
+
+function signinSubmitHandler(event) {
+  event.preventDefault();
+  console.log(event.target);
+  console.log(event.target.username.value);
+  console.log(event.target.password.value);
+}
+
+function notesSubmitHandler(event) {
+  event.preventDefault();
+  console.log(event.target);
+}
+
+const signupForm = document.getElementById('signup-form');
+signupForm.addEventListener('submit', signupSubmitHandler);
+
+const signinForm = document.getElementById('signin-form');
+signinForm.addEventListener('submit', signinSubmitHandler);
+
+const notesForm = document.getElementById('notes');
+notesForm.addEventListener('submit', notesSubmitHandler);
